a1024025ddd2b1013e1920777659d52f9d9a08f3
Andrei Mihaescu <andrei.mihaescu@siteassist.co>
2025-03-27 16:51:36 +0100

    Module 2.1

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <head>
+            <meta name="description" content="Get all your sports fishing equipment at One Place Fishing. Open Monday to Friday, 9 to 5, in the Great Lake area."/>
+            <meta name="og:title" content="Little lemon"/>
+            <meta name="og:description" content="Little lemon restaurant"/>
+            <meta name="og:image" content="public/assets/food.jpeg"/>
+        <h1>Header</h1>
+        </head>
+    );
+}
